349e47f4556783a4ab214594528b5e33ae4bb61e
kazuki3 <okymkzk0816@gmail.com>
2019-06-13 15:49:29 +0900

    Make TopPage Routing

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>HobbyChat</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  </head>
+  <body>
+      @yield('content')
+  </body>
+</html>
