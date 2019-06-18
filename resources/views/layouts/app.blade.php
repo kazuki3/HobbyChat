98e1af034caafac9f83159dbbf4680fb91098f55
kazuki3 <okymkzk0816@gmail.com>
2019-06-18 13:09:26 +0900

    Fix: ルーティング修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,8 +18,15 @@
         <div class="header-nav">
           <nav class="menu">
             <ul>
-              <li><a href="#">ログイン</a></li>
-              <li><a href="#">新規登録</a></li>
+              @if (Auth::check())
+                <li>
+                  <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">ログアウト</a>
+                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">@csrf</form>
+                </li>
+              @else
+                <li><a href="{{ route('login') }}">ログイン</a></li>
+                <li><a href="{{ route('register') }}">新規登録</a></li>
+              @endif
             </ul>
           </nav>
         </div>
@@ -69,11 +76,11 @@
           <div class="CommonMenu_List">
             <ul>
               <li>
-                <div class="name"><span class="MakeIcon"><i class="fas fa-pencil-alt"></i></span>作成</div><a href="#" class="btn-make" title="新しく掲示板を作る">新しく掲示板を作る</a>
+                <div class="name"><span class="MakeIcon"><i class="fas fa-pencil-alt"></i></span>作成</div><a href="{{ route('room') }}" class="btn-make" title="新しく掲示板を作る">新しく掲示板を作る</a>
               </li>
               <li>
                 <form action="" method="post">
-                  <input type="hidden" name="" value="" />
+                  <input type="hidden" name="" value="">
                   <div class="name"><span class="SearchIcon"><i class="fas fa-search"></i></span>検索</div>
                   <input type="text" class="SearchInput" name="keyword" id="" value placeholder="キーワードで検索" />
                 </form>
